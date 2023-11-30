a9b188a96ec53a5b31d4548bb74336f54e3b820f
Luis Masuelli <luis@datagran.io>
2023-11-30 17:06:18 -0500

    Adding the setup.py file.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='simple-stats-challenge',
+    version='0.0.1',
+    packages=['simple_stats'],
+    url='https://github.com/luismasuelli/simple-stats-challenge',
+    license='MIT',
+    author='luismasuelli',
+    author_email='luismasuelli@hotmail.com',
+    description='Computes a dumb set of stats on a set of integer numbers'
+)
